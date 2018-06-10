d77ac20d0b40ea4b5342da6f61c2ca64f9e60c0a
sandeep katta <sandeep.katta@clarks.com>
2018-06-10 19:19:56 +0100

    test

--- features/valtech.feature
+++ features/valtech.feature
@@ -1,8 +1,13 @@
 @search
 Feature: search for valtach
   
-  Scenario : valtech
+  Scenario: valtech
     When I goto "https://www.valtech.com"
     Then I can see "latest news" section
     And I goto "https://www.valtech.com/about" page
-    Then The header section should contain "about"
+    Then the header section should contain "about"
+    And I goto "https://www.valtech.com/services" page
+    Then the header section should contain "services"
+    And I goto "https://www.valtech.com/work" page
+    Then the header section should contain "work"
+    And the no of companies in contact us section are "33"
--- step-definitions/search-steps.js
+++ step-definitions/search-steps.js
@@ -8,13 +8,13 @@ module.exports = function () {
 
     this.Then(/^I can see "([^"]*)" section$/, function (section) {
 
-        return driver.wait(until.elementsLocated(by.css('div.news-post__listing-header')), 10000).then(function() {
+         return driver.wait(until.elementsLocated(by.css('div.news-post__listing-header')), 10000).then(function() {
 
             return driver.findElement(by.css('div.news-post__listing-header .block-header__heading')).getText();
 
         }) .then(function (element) {
 
-        expect(element.toLowerCase()).to.equal(section + "1");
+        expect(element.toLowerCase()).to.equal(section);
 
         });
 
@@ -25,18 +25,29 @@ module.exports = function () {
         return helpers.loadPage(page);
     });
 
-    this.Then(/^The header sxetion should contain "([^"]*)"$/, function (headerText) {
+    this.Then(/^the header section should contain "([^"]*)"$/, function (headerText) {
 
-        return driver.wait(until.elementsLocated(by.css('div.page-header')), 10000).then(function() {
+        return driver.wait(until.elementsLocated(by.css('.page-header')), 10000).then(function() {
 
-            // return the promise of an element to the following then.
-            return driver.findElement(by.css('div.page-header h1')).getText();
+            return driver.findElement(by.css('.page-header h1')).getText();
 
         }) .then(function (element) {
 
-            // verify this element has children
             expect(element.toLowerCase()).to.equal(headerText);
 
+        });
+    });
+
+    this.Then(/^the no of companies in contact us section are "([^"]*)"$/, function (noOfOffices) {
+
+        return driver.wait(until.elementsLocated(by.css('.foot__offices')), 10000).then(function() {
+
+            return driver.findElement(by.css('.foot__offices')).getText();
+
+        }) .then(function (element) {
+
+            var actualOffices = element.split(",").length -1;
+            expect(actualOffices).to.equal(parseInt(noOfOffices));
         });
     });
 
